651aba1dcb2938aa726efe2c6ef35cd1579fa5c0
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-10-11 19:12:23 -0300

    Update DadosPessoais.cs

--- DadosPessoais.cs
+++ DadosPessoais.cs
@@ -1,9 +1,13 @@
 using System;
 
-class MainClass 
+class DadosPessoais 
 {
   public static void Main (string[] args) 
   {
-    
+    public string Nome;
+    public string Idade;
+    public string Cpf;
+    public string Profissão;
+    public int QtdDependentes;
   }
 }
