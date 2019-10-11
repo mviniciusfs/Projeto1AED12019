48776c831884a6c8f4a7f55e4a51fe7d61eb8e6a
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-10-11 19:11:16 -0300

    Update DadosCasa.cs

--- DadosCasa.cs
+++ DadosCasa.cs
@@ -1,9 +1,13 @@
 using System;
 
-class MainClass 
+class DadosCasa
 {
   public static void Main (string[] args) 
   {
-    
+    public string Eletrodomesticos;
+    public string Eletronicos;
+    public string Comodos;
+    public string Garagem;
+    public string Veiculos;
   }
 }
