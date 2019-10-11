085ea77d97f03d9f0d4836b9c09bc6faf441b830
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-10-11 19:11:38 -0300

    Update DadosFamilia.cs

--- DadosFamilia.cs
+++ DadosFamilia.cs
@@ -1,9 +1,13 @@
 using System;
 
-class MainClass 
+class DadosFamilia 
 {
   public static void Main (string[] args) 
   {
+    public int QtdMembros;
+    public int Menores18;
+    public int Maiores18;
+    public string DepDebilitado;
     
   }
 }
