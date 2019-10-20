6eae3e3e9c2a127dc582a542312255baff517fa9
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-10-20 17:55:16 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,9 +1,65 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 class MainClass 
 {
   public static void Main (string[] args) 
   {
-    
+    Administrador adm = new Administrador();
+
+    Console.WriteLine("Cadastro Pessoa baixa renda");
+    Console.WriteLine();
+
+    Console.Write("Deseja cadastrar uma nova pessoa? ");
+    string cad = Console.ReadLine();
+
+    while(cad == "sim" || cad == "SIM")
+    {
+      Console.Write("Digite o Nome: ");
+      string nome = Console.ReadLine();
+
+      Console.Write("Digite o Cargo: ");
+      string cargo = Console.ReadLine();
+
+      Console.Write("Digite a Idade: ");
+      int idade = int.Parse(Console.ReadLine());
+
+      Console.Write("Digite a Quantidade de Filhos: ");
+      int qtdFilhos = int.Parse(Console.ReadLine());
+
+      Console.Write("Digite o Salário: ");
+      double salario = double.Parse(Console.ReadLine());
+
+      
+      
+      adm.AddPessoa(nome, cargo, idade, qtdFilhos, salario);
+      //adm.Imprimir();
+
+      StreamWriter x;
+
+      string CaminhoNome = "dados.txt";
+
+      x = File.AppendText(CaminhoNome);
+
+      //string aux = "";
+
+      List <Pessoa>Pessoas = adm.getListaPessoa();
+      
+      foreach(Pessoa pessoa in  Pessoas)
+      {
+        x.WriteLine(pessoa.Imprimir());
+        
+      }
+
+      
+      x.Close();
+    }  
+
+
+    Console.WriteLine(Controlador.ValidaBeneficio(adm.Pessoas[0]));
+
+    Console.WriteLine(Controlador.ValidaBolsaF(adm.Pessoas[0]));
+
   }
 }
