fcc5e6190e2b2d71f171b638bf9152387bd384b7
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2019-10-20 17:56:27 -0300

    Update Controlador.cs

--- Controlador.cs
+++ Controlador.cs
@@ -2,12 +2,47 @@ using System;
 
 class Controlador 
 {
-  //public static void Main (string[] args) 
-  {
-    public bool ValidaValor;
-    public double MostraValor;
-    public string Beneficio;
-            
+   
+    /*
+    Beneficio:
+    1 - desconto energia
+    */
         
-  }
+    public static string ValidaBeneficio(Pessoa p)
+    {
+      if(p.Salario <= 1000)
+      {
+        return "\nBenefícios disponíveis: \nEnergia 10% \nÁgua 15%";
+      }
+
+      else if (p.Salario >1000 && p.Salario <= 1300)
+      {
+        return "\nBenefícios disponíveis: \nEnergia 5% \nÁgua 8%";
+      }
+
+      else if (p.Salario >1300 && p.Salario <= 1500)
+      {
+        return "\nBenefícios disponíveis: \nÁgua 5%";
+      }
+
+      else
+      {
+        return "\nNão há beneficios disponíveis";
+      }
+        
+    }
+
+    public static string ValidaBolsaF(Pessoa bf)
+    {
+      if(bf.QtdFilhos >= 1)
+      {
+        return "\nPossui direito a auxilio bolsa família por filho";
+      }
+
+      else
+      {
+        return "\nNão possui direito ao auxilio bolsa família.";
+      }
+      
+    } 
 }
